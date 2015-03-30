98fe3eae919915c39e8b0d1fe175d7eb624b23ef
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-29 23:22:15 -0500

    Rought out a proof of concept

--- src/com/danthesalmon/javacogs/ClsRest.java
+++ src/com/danthesalmon/javacogs/ClsRest.java
@@ -1,29 +1,27 @@
 package com.danthesalmon.javacogs;
 
+import java.awt.List;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Iterator;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class ClsRest
 {
    public static String httpGet(String urlStr) throws IOException {
-      URL url = new URL(urlStr);
+      URL url = new URL(urlStr);      
       HttpURLConnection conn =
           (HttpURLConnection) url.openConnection();
+      
+      
       // Set the User-Agent or Discogs will ignore our request and we'll get a big error.
       conn.setRequestProperty("User-Agent", "Test UA");
-
+      System.out.println("Header 1: "+ conn.getHeaderField(0));
+      System.out.println("Header 2: "+ conn.getHeaderField(1));
+      System.out.println("Server: "+conn.getHeaderField("Server"));
+      
       if (conn.getResponseCode() != 200) {
         throw new IOException(conn.getResponseMessage());
       }
@@ -41,9 +39,5 @@ public class ClsRest
       conn.disconnect();
       return sb.toString();
     }
-
-   
-      
-
 }
 
--- src/com/danthesalmon/javacogs/Release.java
+++ src/com/danthesalmon/javacogs/Release.java
@@ -1,11 +1,64 @@
 package com.danthesalmon.javacogs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Release
 {
+   //  VARIABLES
+   // http://www.discogs.com/developers/#page:database,header:database-release-get
+   private String title = "";
+   private Integer id = 0;
+   private String dataQuality = "";
+   private String status = "";
+   
    // Default constructor method
-   public Release()
+   public Release(String releaseID)
    {
-      // TODO Auto-generated constructor stub
+      // Create new JSONParser object
+      JSONParser parser = new JSONParser();
+      
+      try {
+         // Get the JSON response
+         String jsonResp = ClsRest.httpGet("https://api.discogs.com/releases/"+releaseID);
+         System.out.println("Response: "+jsonResp);
+         
+         // Create an object
+         Object obj = parser.parse(jsonResp);
+
+         JSONObject jsonObject = (JSONObject) obj;
+
+         title = (String) jsonObject.get("title");
+         //id = (Integer) jsonObject.get("id").toString();
+         dataQuality = (String) jsonObject.get("data_quality");
+         
+         // loop array
+         //JSONArray msg = (JSONArray) jsonObject.get("messages");
+         //Iterator<String> iterator = msg.iterator();
+         //while (iterator.hasNext()) {
+           //  System.out.println(iterator.next());
+         //}
+
+     } catch (FileNotFoundException e) {
+         e.printStackTrace();
+     } catch (IOException e) {
+         e.printStackTrace();
+     } catch (ParseException e) {
+         e.printStackTrace();
+     }
    }
 
+   //  SETTERS
+   
+   //  GETTERS
+   public String getTitle() {
+      return this.title;
+   }
+
+   public Integer getID(){
+      return this.id;
+   }
 }
